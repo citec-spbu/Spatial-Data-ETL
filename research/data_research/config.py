c213bb3b927e97f22349dc6c9a98e91614725d44
Tigran <75087976+TigranNice@users.noreply.github.com>
2024-04-22 15:33:28 +0300

    Delete research/data_research/config.py

--- research/data_research/config.py
+++
@@ -1,9 +0,0 @@
-russian_regions = ['Central Federal District', 'Crimean Federal District', 'Far Eastern Federal District', 'North Caucasus Federal District', 'Northwestern Federal District',
-                   'Siberian Federal District', 'South Federal District', 'Ural Federal District', 'Volga Federal District']
-file_format = ".pbf"
-data_dir = 'data'
-host = 'localhost'
-port = 5432
-username = 'postgres'
-password = None
-database_name = 'osmdb_test'
