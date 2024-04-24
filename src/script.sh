36acf022889f881d5aaa8bc6f88ca81fc647feb4
potomushozhenya <zhestokanov2017@yandex.ru>
2024-04-24 23:17:04 +0300

    First MVP try

---
+++ src/script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source spatial.config
+DB_NAME=$(echo "${DB_NAME,,}")
+PGPASSWORD=$DB_PSWD
+baseDir=$(pwd)
+cd ~
+echo "$HOST:$PORT:$DB_NAME:$DB_USER:$DB_PSWD" >> .pgpass
+chmod 600 ~/.pgpass
+cd $baseDir
+cd ..
+cd data
+dataDir=$(pwd)
+sudo wget -O $PBF_NAME $LINK
+cd $baseDir
+sudo apt-get update
+if !(which psql>/dev/null)
+then sudo apt install postgresql=14.11
+fi
+res=$(pg_config --version)
+re='[[:digit:]][[:digit:]]\.*'
+psqlVer=14
+for word in $res; do
+        if [[ $word =~ [0-9][0-9]\.* ]]; then
+                psqlVer=$(echo $word | cut -f1 -d.)
+                break
+        fi
+done
+
+fPath=$(sudo find /etc/postgresql -name 'pg_hba.conf')
+sudo sed -i -e 's/md5/peer/g' $fPath
+sudo service postgresql restart
+sleep 15s
+sudo apt install postgis postgresql-$psqlVer-postgis-3
+sudo -u postgres psql -c "create database $DB_NAME;"
+sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION postgis;"
+sudo sed -i -e 's/peer/md5/g' $fPath
+sudo systemctl restart postgresql
+sudo apt install osm2pgsql
+cd $dataDir
+osm2pgsql -U $DB_USER -l -d $DB_NAME $PBF_NAME
