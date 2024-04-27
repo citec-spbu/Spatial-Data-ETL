aee7d0d32e5b423915c035f7c60cde111d35ccdf
potomushozhenya <zhestokanov2017@yandex.ru>
2024-04-28 00:28:02 +0300

    Another fix

--- src/script.sh
+++ src/script.sh
@@ -12,26 +12,15 @@ cd ../data
 dataDir=$(pwd)
 sudo wget -O $PBF_NAME $LINK
 cd $baseDir
-sudo apt-get update
-if !(which psql>/dev/null)
-then sudo apt install postgresql=14.11
-fi
+sudo apt-get --yes --force-yes update
 res=$(pg_config --version)
 re='[[:digit:]][[:digit:]]\.*'
-psqlVer=14
-for word in $res; do
-        if [[ $word =~ [0-9][0-9]\.* ]]; then
-                psqlVer=$(echo $word | cut -f1 -d.)
-                break
-        fi
-done
-
+sudo apt install postgis postgresql-14-postgis-3
 fPath=$(sudo find /etc/postgresql -name 'pg_hba.conf')
 sudo sed -i -e 's/md5/trust/g' $fPath
 sudo sed -i -e 's/peer/trust/g' $fPath
 sudo service postgresql restart
 sleep 15s
-sudo apt install postgis postgresql-$psqlVer-postgis-3
 sudo -u postgres psql -c "create database $DB_NAME;"
 sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION postgis;"
 sudo apt install osm2pgsql
