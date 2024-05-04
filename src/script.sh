9b80e92ae553a47243bd3892e2cb1aa353a2bb7d
potomushozhenya <zhestokanov2017@yandex.ru>
2024-05-05 00:57:17 +0300

    New feature

--- src/script.sh
+++ src/script.sh
@@ -10,12 +10,10 @@ chmod 600 ~/.pgpass
 cd $baseDir
 cd ../data
 dataDir=$(pwd)
-sudo wget -O $PBF_NAME $LINK
 cd $baseDir
 sudo apt-get -y --allow-change-held-packages --allow-remove-essential update
-res=$(pg_config --version)
-re='[[:digit:]][[:digit:]]\.*'
 sudo apt -y --allow-change-held-packages --allow-remove-essential install postgis postgresql-14-postgis-3
+sudo apt install osm2pgsql
 fPath=$(sudo find /etc/postgresql -name 'pg_hba.conf')
 sudo sed -i -e 's/md5/trust/g' $fPath
 sudo sed -i -e 's/peer/trust/g' $fPath
@@ -23,6 +21,12 @@ sudo service postgresql restart
 sleep 15s
 sudo -u postgres psql -c "create database $DB_NAME;"
 sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION postgis;"
-sudo apt install osm2pgsql
 cd $dataDir
-osm2pgsql -U $DB_USER -l -d $DB_NAME $PBF_NAME
+while IFS= read -r line; do
+    line=$(echo "$line" | sed 's/\r$//')
+    name=$(echo ${line} | awk -F'/' '{print $5}')
+    name=$(echo ${name} | awk -F'-' '{print $1}')
+    nameFile=$"$name.osm.pbf"
+    sudo wget -O $nameFile $line
+    osm2pgsql -U $DB_USER -p $name -l -d $DB_NAME $nameFile
+done < ../src/links.config
