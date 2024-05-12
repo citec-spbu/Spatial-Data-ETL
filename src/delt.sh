4a143c02d2c167b7ec1cb78d0d6cb86811dc1d99
potomushozhenya <zhestokanov2017@yandex.ru>
2024-05-12 17:20:14 +0300

    links.config read fix and osmosis init fix

--- src/delt.sh
+++ src/delt.sh
@@ -2,11 +2,12 @@
 source spatial.config
 cd ..
 baseDir=$(pwd)
-deltDir="$baseDir/deltas"
-WORKOSM_DIR="/home/admin/bashDeploy/deltas"
-osmosis --read-replication-interval-init workingDirectory=$WORKOSM_DIR
+deltDir=$"${baseDir}/deltas"
 cd $deltDir
-while IFS= read -r line; do
+sudo chmod a+w .
+rm -f configuration.txt
+osmosis --read-replication-interval-init workingDirectory=$deltDir
+while IFS= read -r line || [[ -n "$line" ]]; do
     line=$(echo "$line" | sed 's/\r$//')
     if [[ -z "$line" ]]; then
         break
@@ -26,6 +27,6 @@ while IFS= read -r line; do
     newUrl="https://download.geofabrik.de/russia/"$name"fed-district-updates/"
     echo $newUrl
     sed -i "/^baseUrl=/c\baseUrl=$newUrl" configuration.txt
-    osmosis --read-replication-interval workingDirectory="${WORKOSM_DIR}" --simplify-change --write-xml-change - | \
+    osmosis --read-replication-interval workingDirectory="${deltDir}" --simplify-change --write-xml-change - | \
     osm2pgsql --append -r xml -s -C 300 -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua -d $DB_NAME -U $DB_USER -
 done < ../src/links.config
--- src/init.sh
+++ src/init.sh
@@ -8,8 +8,9 @@ cd ~
 echo $DB_HOST:$DB_PORT:*:$DB_USER:$DB_PSWD%> .pgpass
 chmod 600 ~/.pgpass
 cd $baseDir
-deltDir="$baseDir/deltas"
-cd ../data
+cd ..
+deltDir="$(pwd)/deltas"
+cd data
 dataDir=$(pwd)
 cd $baseDir
 sudo apt-get -y --allow-change-held-packages --allow-remove-essential update
@@ -24,11 +25,14 @@ sleep 15s
 sudo -u postgres psql -c "create database $DB_NAME;"
 sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION postgis;"
 sudo -u postgres psql -d $DB_NAME -c "CREATE EXTENSION hstore;"
-cd deltDir
+cd $deltDir
 #У регионов одинаковый state.txt, поэтому можно любой поставить
 sudo wget -O state.txt https://download.geofabrik.de/russia/central-fed-district-updates/state.txt
 cd $dataDir
-while IFS= read -r line; do
+while IFS= read -r line || [[ -n "$line" ]]; do
+    if [[ -z "$line" ]]; then
+        break
+    fi
     line=$(echo "$line" | sed 's/\r$//')
     wordsArr=$(echo ${line} | awk -F'/' '{print $5}')
     words=$(echo $wordsArr | tr "-" "\n")
