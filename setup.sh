fdf7058096606acce9f0fc0e4ca828b638ab20a3
potomushozhenya <zhestokanov2017@yandex.ru>
2024-05-20 20:07:05 +0300

    DAG file path fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+mkdir data
+mkdir deltas
 dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd src
 path="${dir}/src/delt.sh "
-sed -i -e "s|PATH_TO_SCRIPT|$path|g" /airflow.py
+sed -i -e "s|PATH_TO_SCRIPT|$path|g" airflow.py
 sudo chmod +x init.sh
 sudo chmod +x delt.sh
 sudo chmod +x newFile.sh
